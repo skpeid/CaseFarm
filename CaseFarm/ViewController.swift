b2cd3afe51a010783688fe3808b82a8d59569e96
Kuanysh <jolydots@gmail.com>
2025-07-03 09:36:03 +0500

    Initial Commit

---
+++ CaseFarm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CaseFarm
+//
+//  Created by Kuanysh Auyelgazy on 03.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
